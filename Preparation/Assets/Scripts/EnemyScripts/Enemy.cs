dff433ea91c1e4811a54b74d6e7f620e157a6088
Myles Williams <mylesfrancisw@gmail.com>
2023-12-13 12:51:17 -0800

    included additional fixes for camera scene transfers

--- Preparation/Assets/Scripts/EnemyScripts/Enemy.cs
+++ Preparation/Assets/Scripts/EnemyScripts/Enemy.cs
@@ -12,7 +12,10 @@ public enum EnemyState{
 
 public class Enemy : MonoBehaviour
 {
+    [Header("State Machine")]
     public EnemyState currentState;
+
+    [Header("Enemy Stats")]
     public FloatValue maxHealth;
     public float health;
     public string enemyName;
@@ -21,6 +24,7 @@ public class Enemy : MonoBehaviour
 
     [Header("Death Effects")]
     public GameObject deathEffect;
+    private float deathEffectDelay = 1f;
 
     private void Awake()
     {
@@ -44,7 +48,7 @@ public class Enemy : MonoBehaviour
         if (deathEffect != null)
         {
             GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
-            Destroy(effect, 1f);
+            Destroy(effect, deathEffectDelay);
         }
     }
 
